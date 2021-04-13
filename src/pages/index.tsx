b121ed8d81fe40931cce8ece32d14d45fd13b140
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-04-13 16:41:49 -0300

    feat: integrate markers with graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import dynamic from 'next/dynamic';
-import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline';
+import { GetStaticProps } from 'next';
 
-import LinkWrapper from 'components/LinkWrapper';
+import HomeTemplate from 'templates/Home';
+import { MapProps } from 'components/Map';
+import client from 'graphql/client';
+import { GET_PLACES } from 'graphql/queries';
+import { GetPlacesQuery } from 'graphql/generated/graphql';
 
-const Map = dynamic(() => import('components/Map'), {
-  ssr: false
-});
-
-function Home() {
-  return (
-    <>
-      <LinkWrapper href="/about">
-        <InfoOutline size={32} aria-label="About" />
-      </LinkWrapper>
-      <Map />
-    </>
-  );
+export default function Home({ places }: MapProps) {
+  return <HomeTemplate places={places} />;
 }
 
-export default Home;
+export const getStaticProps: GetStaticProps = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES);
+
+  return {
+    props: {
+      places
+    }
+  };
+};
