0582a43df942a9395b1c69bdb3e98233883c23db
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-04-13 21:17:20 -0300

    feat: config seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import NextNprogress from 'nextjs-progressbar';
+import { DefaultSeo } from 'next-seo';
 
+import SEO from '../../next-seo.config';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
@@ -10,7 +12,6 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -20,11 +21,8 @@ function App({ Component, pageProps }: AppProps) {
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossOrigin=""
         />
-        <meta
-          name="description"
-          content="A simple project to show my favorites spots in the world"
-        />
       </Head>
+      <DefaultSeo {...SEO} />
       <GlobalStyles />
       <NextNprogress
         color="#f231a5"
