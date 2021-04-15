74b85dcd1cb8fa2f50c380a97eeee14ebc6d28c7
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-04-15 10:18:20 -0300

    chore: add revalidate pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const { places } = await client.request<GetPlacesQuery>(GET_PLACES);
 
   return {
+    revalidate: 60 * 60 * 24,
     props: {
       places
     }
